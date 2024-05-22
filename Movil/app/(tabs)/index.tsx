dc6e8ae1114fa0a8949e3754b4c0744f1e4b167a
Padilla <paca23919@gmail.com>
2024-05-22 10:41:57 -0600

    cabios en los componentes

--- Movil/app/(tabs)/index.tsx
+++ Movil/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, Platform } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
+import { StyleSheet } from 'react-native';
 import Clima from '@/components/Clima';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -11,53 +10,10 @@ export default function HomeScreen() {
       backgroundColor="#f0f0f0"
       contentBackgroundColor="#fff"
       parallaxHeaderHeight={300}
-      renderForeground={() => (
-        <ThemedView style={styles.headerContainer}>
-          <HelloWave />
-          <ThemedText style={styles.headerText}>Welcome to HomeScreen</ThemedText>
-        </ThemedView>
-      )}
+      renderForeground={() => null} // No necesitamos un encabezado foreground
     >
-      <ThemedView style={styles.contentContainer}>
-        <ThemedText style={styles.titleContainer}>Title</ThemedText>
-        <ThemedText style={styles.stepContainer}>Step 1</ThemedText>
-        <ThemedText style={styles.stepContainer}>Step 2</ThemedText>
-        <Image
-          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
-          style={styles.reactLogo}
-        />
-      </ThemedView>
+
     </Clima>
   );
 }
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100%',
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  contentContainer: {
-    padding: 16,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+;
---
+++ Movil/components/Estilos.tsx
@@ -0,0 +1,64 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  datos: {
+    color: 'white',
+  },
+  datosBold: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  title: {
+    color: '#AFCAF3',
+    fontWeight: 'bold',
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  texto: {
+    color: '#AFCAF3',
+  },
+  container: {
+    backgroundColor: '#4183D1',
+    marginHorizontal: 15,
+    borderRadius: 20,
+    padding: 15,
+    marginBottom: 10,
+  },
+  hContainer: {
+    marginRight: 25,
+    alignItems: 'center',
+  },
+  vContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  datosContainer: {
+    alignItems: 'center',
+    marginBottom: 50, // Ajusta este valor según sea necesario
+    alignContent: 'space-between',
+    padding: 10,
+  },
+  lugar: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  temperatura: {
+    color: 'white',
+    fontSize: 80,
+  },
+  headerContainer: {
+    alignItems: 'center',
+    paddingVertical: 10, // Ajusta este valor según sea necesario
+    marginTop: -250, // Ajusta este valor para mover hacia arriba
+  },
+  headerTemperatureContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: -10, // Ajusta este valor según sea necesario
+  },
+  contentContainer: {
+    padding: 16,
+  },
+});
